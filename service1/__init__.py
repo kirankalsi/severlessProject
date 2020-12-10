b314c5204ea96841ca765a98d354520bae7ed436
Ubuntu <kiran@vm1.xroaw2pvl5juzcf2u203330zhd.zx.internal.cloudapp.net>
2020-12-10 15:48:29 +0000

    all services working

--- service1/__init__.py
+++ service1/__init__.py
@@ -6,7 +6,10 @@ import requests
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    numstring = requests.get().text
-    letterstring = requests.get().text
+    numstring = requests.get('https://serverlessproject.azurewebsites.net/api/service2?code=SJYtXl8T8CgYuOpJcTi3v024/7CtCA6JQA/YM5wYLJsQPPa/zKN3gg==').text
+    letterstring = requests.get('https://serverlessproject.azurewebsites.net/api/service3?code=wLj0EOvK4KivawcmHPuWshg3QifygU3flAJKEYaJnh723aHRf53pLw==').text
 
-    username = ''.join(numstring + letterstring) for i in range(5))
+    username = ""
+    for i in range(5):
+        username += numstring[i] + letterstring[i]
+    return username
